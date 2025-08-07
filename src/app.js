4613c6d4ee4a8eca1807d179cc4c269bafeaefdd
gdangga <gedeangga424@gmail.com>
2025-08-07 20:12:31 +0800

    feat:create products API

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json()); // Middleware untuk parsing JSON body
+const productRoutes = require('./routes/productRoutes');
+
+app.use('/api/products', productRoutes);
+
+
+// Rute untuk setiap modul
+
+
+// Rute dasar
+app.get('/', (req, res) => {
+   res.send('Welcome to the E-commerce API!');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+   console.log(`Access API at http://localhost:${PORT}/api`);
+});
